7a5a4e3095fec1c38e7c3b5e9032e85515be71e1
Sanchie2003 <sanchieshamin@gmail.com>
2026-08-20 03:28:26 +0300

    Fix PORT definition in server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -13,6 +12,7 @@ app.get('/', (req, res) => {
 const paymentsRouter = require('./payments');
 app.use('/api/payments', paymentsRouter);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at port ${PORT}`);
 });
