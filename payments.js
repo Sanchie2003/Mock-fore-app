7b64c57c3868ed93d5b71b47e52caebeccfef6b0
Sanchie2003 <sanchieshamin@gmail.com>
2026-08-20 03:30:43 +0300

    Make payments.js a proper express router

--- payments.js
+++ payments.js
@@ -1,1 +1,8 @@
-module.exports = { createPaymentIntent: async () => ({ id: 'mock_123' }) };
+const express = require('express');
+const router = express.Router();
+
+router.post('/create-payment-intent', async (req, res) => {
+    res.json({ clientSecret: 'mock_secret_123' });
+});
+
+module.exports = router;
